10982903d7e745ef97c459151992e3ccadfa6219
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-01-11 00:25:02 +0200

    Adding table view unit testing

--- src/app/views/table/table.component.spec.ts
+++ src/app/views/table/table.component.spec.ts
@@ -1,17 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { SpinnerComponentModule } from '@components/spinner/spinner.module';
+import { RandomUserNationalitiesService } from '@services/random-user-nationalities/random-user-nationalities.service';
+import { Subscription } from 'rxjs';
 import { TableComponent } from './table.component';
 
 describe('TableComponent', () => {
   let component: TableComponent;
   let fixture: ComponentFixture<TableComponent>;
+  let randomUserNationalitiesService: RandomUserNationalitiesService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TableComponent ]
+  beforeEach(
+    waitForAsync(() => {
+      {
+        TestBed.configureTestingModule({
+          imports: [HttpClientTestingModule, SpinnerComponentModule],
+          declarations: [TableComponent],
+          providers: [RandomUserNationalitiesService],
+        }).compileComponents();
+        randomUserNationalitiesService = TestBed.inject(
+          RandomUserNationalitiesService
+        );
+      }
     })
-    .compileComponents();
-  });
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TableComponent);
@@ -21,5 +33,22 @@ describe('TableComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getRandomUsers on the component creation', () => {
+    const spy = spyOn(component, 'getRandomUsers');
+
+    component.ngOnInit();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should remove random User subscription on the component destroyed', () => {
+    component.randomUserSub = new Subscription();
+    const spy = spyOn(component.randomUserSub, 'unsubscribe');
+
+    component.ngOnDestroy();
+
+    expect(spy).toHaveBeenCalled();
   });
 });
